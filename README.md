017911d7e5af75139f7686b507cd65f9d52855f9
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-04-04 15:37:27 -0500

    Create initial files and grid
    Boiler plates made, all files linked, and made grid with js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TOP-etch-a-sketch
 Odin project: https://www.theodinproject.com/lessons/foundations-etch-a-sketch
+
+Second to last project for Odin" Fundametals". 
---
+++ javascript.js
@@ -0,0 +1,49 @@
+// Declare grid
+const gridContainer = document.querySelector("#gridContainer");
+
+// Create a row
+function createRow() {
+    const row = document.createElement("div");
+    createSquares(row);
+    gridContainer.appendChild(row);
+
+    row.style.display = "flex";
+    row.style.marginBottom = "1px";
+    row.style.justifyContent = "center";
+
+
+}
+
+// Create 16 squares
+function createSquares(rowDiv) {
+    for(i = 0; i < 16; i++) {
+        const square = document.createElement("div");
+        square.style.height = "20px";
+        square.style.width = "20px";
+        rowDiv.appendChild(square);
+    }
+}
+
+// loop for 16x16 grid
+for(j = 0; j < 16; j++) {
+    createRow();
+}
+
+
+
+
+
+
+// const btnTest = document.createElement("button");
+// btnTest.style.height = "15px";
+// btnTest.style.backgroundColor = "blue";
+// document.querySelector("body").appendChild(btnTest);
+// btnTest.addEventListener("click", createRow);
+
+
+// for(i = 0; i < 5; i++) {
+//     console.log(777);
+// }
+
+console.log("end");
+
